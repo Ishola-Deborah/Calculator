1d472d9f60cf16e6afc269fde53123935cf0eb73
DebbDev-cloud <isholadeborahope@gmail.com>
2021-11-05 14:52:59 +0100

    complete calculator using html,css,js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function getOutput() {
     return document.getElementById("output-value").innerText;
 }
 function printOutput(num) {
-    if (num === "") {
+    if (num == "") {
         document.getElementById("output-value").innerText=num;
     }
     else {
