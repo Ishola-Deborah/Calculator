7ccacf522e2defa44f3ebf756bd202f5df914a64
DebbDev-cloud <isholadeborahope@gmail.com>
2021-11-06 12:58:24 +0100

    complete calculator

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ var number = document.getElementsByClassName("number");
 for (var i = 0; i < number.length; i++) {
     number [i].addEventListener('click', function(){
         var output = reverseNumberFormat(getOutput());
-        if (output!==NaN) { // if output is a number
+        if (output!==NaN) { // if output is a
             output = output + this.id;
             printOutput(output);
         }
